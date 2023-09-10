91c568e87c6b951c34b11c77da810096e6945a30
nathan <nathanj758@gmail.com>
2023-09-10 03:39:48 -0400

    enable reticle in insert mode

--- lua/custom/plugins/reticle.lua
+++ lua/custom/plugins/reticle.lua
@@ -5,6 +5,7 @@ return {
     'tummetott/reticle.nvim',
     config = function()
         require('reticle').setup {
+            disable_in_insert = false,
 
             -- add options here or leave empty
         }
