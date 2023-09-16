bdfc14c1f51d943b6f6b7c8d83ca87e74eefce9c
nathan <nathanj758@gmail.com>
2023-09-16 03:59:10 -0400

    rename x to give more clarity

--- lua/custom/plugins/easy_buf_deleter.lua
+++ lua/custom/plugins/easy_buf_deleter.lua
@@ -1,11 +1,11 @@
 return {
     vim.api.nvim_create_user_command("BdAllOthers",
 	function(opts)
-	    local x = vim.api.nvim_list_bufs()
+	    local buffer_list = vim.api.nvim_list_bufs()
 	    local current = vim.api.nvim_get_current_buf()
-	    for i = 1, #x do
-	      if x[i] ~= current then
-		    vim.api.nvim_buf_delete(x[i], {})
+	    for i = 1, #buffer_list do
+	      if buffer_list[i] ~= current then
+		    vim.api.nvim_buf_delete(buffer_list[i], {})
 	      end
 	    end
 	end,
